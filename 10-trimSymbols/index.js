ee34d03e9894e5429972e34244a765acc8e37089
AlexBelousUa <90335588+AlexBelousUa@users.noreply.github.com>
2021-09-09 19:54:46 +0300

    Update index.js

--- 10-trimSymbols/index.js
+++ 10-trimSymbols/index.js
@@ -5,5 +5,19 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
+let result = '';
+let newArray = string.split('');
+let mainArray = [...new Set(newArray)];
+let keys = 0;
+for(i = 0; i < mainArray.length; i++ ){
 
+for(j = 0; j < newArray.length; j++ ){
+if(newArray[j] == mainArray[i]) {result = result + newArray[j]; keys = keys + 1; };
+if(keys == size){ keys = 0; break; };
 }
+return result;
+}
+
+
+
+
